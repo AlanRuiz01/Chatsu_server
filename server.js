8d9659284ff99cc5636e8eccc95c677acc023589
Alan Ruiz <matiasruizc315@gmail.com>
2024-07-22 21:07:01 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: '*',
+  },
+});
+
+app.use(cors());
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  socket.on('message', (data) => {
+    io.emit('message', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
