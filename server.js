189221fcca440eaa60c8c04dfb2f29be63b8c3bb
Alan Ruiz <matiasruizc315@gmail.com>
2024-07-24 15:17:56 -0400

    Server changes

--- server.js
+++ server.js
@@ -16,8 +16,20 @@ app.use(cors());
 io.on('connection', (socket) => {
   console.log('New client connected');
 
-  socket.on('message', (data) => {
-    io.emit('message', data);
+  socket.on('joinGroup', (groupId) => {
+    socket.join(groupId);
+    console.log(`Client joined group ${groupId}`);
+  });
+
+  socket.on('leaveGroup', (groupId) => {
+    socket.leave(groupId);
+    console.log(`Client left group ${groupId}`);
+  });
+
+  socket.on('sendMessage', (data) => {
+    const { message, userName, timestamp, groupId } = data;
+    console.log('Message received on server:', data); // Verifica los datos recibidos
+    io.to(groupId).emit('message', { message, userName, timestamp, groupId });
   });
 
   socket.on('disconnect', () => {
